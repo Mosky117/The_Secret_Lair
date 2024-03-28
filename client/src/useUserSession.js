8e98ab2e9281a20b7d87bb3e1f1264fd835fff5f
Mosky117 <davi.mosky@outlook.com>
2024-03-28 17:53:16 +0100

    .

--- client/src/useUserSession.js
+++ client/src/useUserSession.js
@@ -1,6 +1,7 @@
 import {useState} from "react";
 
 export default function useUserSession(){
+    //using local storage to check token
     const saveToken=localStorage.getItem('token');
     const [userToken, setToken]=useState(saveToken);
 
@@ -8,7 +9,7 @@ export default function useUserSession(){
         localStorage.removeItem('token');
         setToken(null);
     }
-    
+
     function saveUserSession(token, callback){
         localStorage.setItem('token', token);
         setToken(token, callback);
