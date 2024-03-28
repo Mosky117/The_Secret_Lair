82784d250b98121ef6ee0febd41cd59ef7d21cb1
Mosky117 <davi.mosky@outlook.com>
2024-03-28 13:37:23 +0100

    .

--- server/index.js
+++ server/index.js
@@ -26,6 +26,7 @@ app.use((req, res, next) => {
 app.use(cors({
     origin: [process.env.REACT_APP_URL],
     methods: ['GET','POST','PUT','DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
 
